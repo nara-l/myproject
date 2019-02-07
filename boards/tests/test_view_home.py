0f571232aa90f21b6da04b4b024045d7d05451bb
lnara <njielitumbe@gmail.com>
2019-02-07 14:44:29 -0500

    Re-initialized git for whole project
    - All commits and messages deleted :(

---
+++ boards/tests/test_view_home.py
@@ -0,0 +1,29 @@
+from django.core.urlresolvers import reverse
+from django.urls import resolve
+from django.test import TestCase
+from ..views import BoardListView
+from ..models import Board
+
+# Create your tests here.
+
+
+class HomeTests(TestCase):
+
+    def setUp(self):
+        self.board = Board.objects.create(name='Django', description='Django board.')
+        url = reverse('home')
+        self.response = self.client.get(url)
+
+    def test_home_view_status_code(self):
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_url_resolves_home_view(self):
+        view = resolve('/')
+        self.assertEquals(view.func.view_class, BoardListView)
+
+    def test_home_view_contains_link_to_topics_page(self):
+        board_topics_url = reverse('board_topics', kwargs={'pk': self.board.pk})
+        self.assertContains(self.response, 'href="{0}"'.format(board_topics_url))
+
